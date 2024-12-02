5e4e6d943aee8691439c0b1c94ba0ac50ede29f8
Marco Mechini <mechinimarco35@gmail.com>
2024-12-02 16:23:07 +0100

    feat: postman work

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const post = require('./data');
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
+//nella route default risponde server del mio blog
 app.get('/', (req, res) => {
     res.send('Server del mio blog')
 })
 
+//nella route bacheca abbiamo come risposta un json che ha come parametri post e la lunghezza
+app.get(`/bacheca`, (req, res) => {
+    res.json({
+        posts: post.data,
+        length: post.data.length,
+    })
+
+})
+
+//logga server in ascolto su terminale
 app.listen(port, () => {
     console.log('Server in ascolto');
 })
